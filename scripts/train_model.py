2a17cdfd0a0f0a3add515a8c474a3faefc021dce
Ebrahim <efeghhi@gmail.com>
2025-03-20 15:31:12 -0700

    removed articulatory code

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,19 +1,19 @@
 
-modelName = 'speechBaseline1'
+modelName = 'speechmodel2'
 
 args = {}
-args['outputDir'] = '/Users/ebrahimfeghhi/neural_seq_decoder/output/' + modelName
-args['datasetPath'] = '/Users/ebrahimfeghhi/neural_seq_decoder/data/ptDecoder_ctc'
+args['outputDir'] = '/data/willett_data/outputs/' + modelName
+args['datasetPath'] = '/data/willett_data/ptDecoder_ctc'
 args['seqLen'] = 150
 args['maxTimeSeriesLen'] = 1200
 args['batchSize'] = 64
 args['lrStart'] = 0.02
 args['lrEnd'] = 0.02
 args['nUnits'] = 1024
-args['nBatch'] = 10000 #3000
+args['nBatch'] = 30000 #3000
 args['nLayers'] = 5
 args['seed'] = 0
-args['nClasses_list'] = [40, 3, 10, 7, 4, 4, 3] # add one to the classes for SIL
+args['nClasses'] = 40
 args['nInputFeatures'] = 256
 args['dropout'] = 0.4
 args['whiteNoiseSD'] = 0.8
@@ -21,11 +21,11 @@ args['constantOffsetSD'] = 0.2
 args['gaussianSmoothWidth'] = 2.0
 args['strideLen'] = 4
 args['kernelLen'] = 32
-args['bidirectional'] = True
+args['bidirectional'] = False
 args['l2_decay'] = 1e-5
+args['device'] = 'cuda:1'
+args['max_mask_channels'] = 0 # within each grid 
 
-import sys
-sys.path.append('/Users/ebrahimfeghhi/neural_seq_decoder/src/')
-from neural_decoder.neural_decoder_trainer_articulation import trainModel
+from neural_decoder.neural_decoder_trainer import trainModel
 
 trainModel(args)
