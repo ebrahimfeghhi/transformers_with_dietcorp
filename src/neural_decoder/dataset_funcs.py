c9d74159c26dc97104ebff0d074492977e5d04e6
Ebrahim <efeghhi@gmail.com>
2025-03-24 14:56:32 -0700

    Added code for dynamic batching

--- src/neural_decoder/dataset_funcs.py
+++ src/neural_decoder/dataset_funcs.py
@@ -43,3 +43,39 @@ def convert_to_phonemes(transcript):
     seqClassIDs = np.zeros([maxSeqLen]).astype(np.int32)
     seqClassIDs[0:seqLen] = [phoneToId(p) + 1 for p in phonemes]
     return seqClassIDs, len(phonemes)
+
+
+
+# Dynamic Batch Sampler with Bucketing
+class DynamicBatchSampler:
+    def __init__(self, lengths, batch_size, shuffle=True, bucket_size=1000):
+        self.lengths = lengths
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.bucket_size = bucket_size
+
+        # Create buckets based on length ranges
+        self.buckets = {}
+        for idx, length in enumerate(lengths):
+            bucket_id = length // bucket_size
+            if bucket_id not in self.buckets:
+                self.buckets[bucket_id] = []
+            self.buckets[bucket_id].append(idx)
+
+    def __iter__(self):
+        if self.shuffle:
+            for bucket in self.buckets.values():
+                np.random.shuffle(bucket)
+
+        batches = []
+        for bucket in self.buckets.values():
+            for i in range(0, len(bucket), self.batch_size):
+                batches.append(bucket[i:i + self.batch_size])
+
+        if self.shuffle:
+            np.random.shuffle(batches)
+
+        return iter(batches)
+
+    def __len__(self):
+        return sum(len(bucket) // self.batch_size for bucket in self.buckets.values())
