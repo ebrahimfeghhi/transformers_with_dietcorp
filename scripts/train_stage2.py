a88d34a1f653f11f79b0eef98c4ac937700e0e15
Ebrahim <efeghhi@gmail.com>
2025-03-24 19:25:23 -0700

    Added dynamic batching

--- scripts/train_stage2.py
+++ scripts/train_stage2.py
@@ -23,7 +23,7 @@ args['strideLen'] = 4
 args['kernelLen'] = 32
 args['bidirectional'] = False
 args['l2_decay'] = 1e-5
-args['device'] = 'cuda:1'
+args['device'] = 'cuda:0'
 
 args['latentspeech_dim_stage2'] = 1024
 args['nLayers_stage2'] = 3
@@ -35,6 +35,9 @@ args['dropout_stage2'] = 0.4
 args['outputdim_stage2'] = 256
 args['strideLen_stage2'] = 1
 args['kernelLen_stage2'] = 4
+args["bucket_size"] = 8000 # half a second of data (audio is in 16Khz)
+args["min_samples_in_bucket"] = 16
+
 
 from neural_decoder.speech2neural import trainModel
 
