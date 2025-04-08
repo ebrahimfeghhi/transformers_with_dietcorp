c1ab4d332e4b8fed4959d9a4ab3583ba7cf0de2b
Ebrahim <efeghhi@gmail.com>
2025-04-07 18:21:38 -0700

    MAE

--- scripts/train_mae.py
+++ scripts/train_mae.py
@@ -6,28 +6,27 @@ args['outputDir'] = '/data/willett_data/outputs/' + modelName
 args['datasetPath'] = '/data/willett_data/ptDecoder_ctc'
 
 args['patch_size']= (4, 16) #TODO
-args['dim'] = 4*16 #TODO
+args['dim'] = 64 #TODO
 args['depth'] = 6 #TODO
-args['heads'] = 10
+args['heads'] = 4
 args['mlp_dim_ratio'] = 4 #TODO
-args['dim_head'] = 32
+args['dim_head'] = 16
 args['dropout'] = 0.1
 
 args['decoder_dim'] = 64
-args['encoder_dim'] = 64
 args['masking_ratio'] = 0.75
 args['num_decoder_layers'] = 2 #TODO
-args['num_decoder_heads'] = 8
-args['num_decoder_dim_head'] = 32
+args['num_decoder_heads'] = 4
+args['num_decoder_dim_head'] = 16
 
 args['batchSize'] = 8
 
 args['weight_decay'] = 1e-5
 args['learning_rate'] = 0.005
-args['num_epochs'] = 5
+args['num_epochs'] = 10000
 
 
-args['device'] = 'cuda:2'
+args['device'] = 'cuda:0'
 
 from neural_decoder.mae_main import trainModel
 
