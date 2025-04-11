f7012bebd2f9ef7e3a297dff7636dff81dc06351
ebrahimfeghhi <efeghhi@gmail.com>
2025-04-10 20:03:44 -0700

    Moving along

--- scripts/train_mae.py
+++ scripts/train_mae.py
@@ -5,26 +5,27 @@ possiblePath_dir = ['/data/willett_data/outputs/',
                     '/home3/skaasyap/willett/outputs/']
 possiblePaths_data = ['/data/willett_data/ptDecoder_ctc', 
                       '/home3/skaasyap/willett/data', 
-                      '/home3/skaasyap/willett/data_log']
+                      '/home3/skaasyap/willett/data_log', 
+                      '/home3/skaasyap/willett/data_log_both']
 
 args = {}
 args['outputDir'] = possiblePath_dir[1] + modelName
-args['datasetPath'] = possiblePaths_data[2]
+args['datasetPath'] = possiblePaths_data[3]
 
 args['trial_size'] = (32, 256)
 args['patch_size']= (32, 2) #TODO
 args['dim'] = 64 #TODO
-args['depth'] = 10 #TODO
+args['depth'] = 12 #TODO
 args['heads'] = 4
 args['mlp_dim_ratio'] = 4 #TODO
-args['dim_head'] = 8
+args['dim_head'] = 16
 args['dropout'] = 0.1
 
 args['decoder_dim'] = 64
-args['masking_ratio'] = 0.5
-args['num_decoder_layers'] = 2 #TODO
-args['num_decoder_heads'] = 8
-args['decoder_dim_head'] = 4
+args['masking_ratio'] = 0.7
+args['num_decoder_layers'] = 3 #TODO
+args['num_decoder_heads'] = 4
+args['decoder_dim_head'] = 16
 
 args['batchSize'] = 275
 
@@ -33,9 +34,13 @@ args['learning_rate'] = 1e-3
 args['num_epochs'] = 10000
 args['gaussianSmoothWidth'] = 2.0
 
-args['extra_notes'] = 'Added day specific layer, added block shuffling.'
+args['day_specific'] = False
+args['day_specific_tokens'] = False
 
-args['device'] = 'cuda:3'
+
+args['extra_notes'] = ("")
+
+args['device'] = 'cuda:0'
 
 from neural_decoder.mae_main import trainModel
 
