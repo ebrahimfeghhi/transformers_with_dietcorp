88d2de8a405348237879e9266ec12c090859a910
ebrahimfeghhi <efeghhi@gmail.com>
2025-04-28 21:15:40 -0700

    See if I can improve the GRU

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 
-modelName = 'unidirectional_gru_baseline'
+modelName = 'unidirectional_gru_with_adamw'
 
 possiblePath_dir = ['/data/willett_data/outputs/', 
                     '/home3/skaasyap/willett/outputs/']
@@ -26,8 +26,8 @@ if os.path.exists(args['outputDir']):
 args['seqLen'] = 150
 args['maxTimeSeriesLen'] = 1200
 args['batchSize'] = 64
-args['lrStart'] = 0.02
-args['lrEnd'] = 0.02
+args['lrStart'] = 0.001
+args['lrEnd'] = 0.001
 args['nUnits'] = 1024
 args['n_epochs'] = 2000 #3000
 args['nLayers'] = 5
@@ -48,10 +48,13 @@ args['nDays'] = 24
 args['testing_on_held_out'] = False
 args['restricted_days'] = []
 args['maxDay'] = None
-args['AdamW'] = False
+args['AdamW'] = True
+args['beta1'] = 0.90
+args['beta2'] = 0.999
 args['learning_scheduler'] = 'None'
 args['load_pretrained_model'] = ''
 args['consistency'] = False
+
 
 from neural_decoder.neural_decoder_trainer import trainModel
 from neural_decoder.model import GRUDecoder
