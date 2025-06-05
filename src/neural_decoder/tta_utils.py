8f681b42b94948343a1bb138d417ba5b717c5c35
ebrahimfeghhi <efeghhi@gmail.com>
2025-06-04 22:45:23 -0700

    woo

--- src/neural_decoder/tta_utils.py
+++ src/neural_decoder/tta_utils.py
@@ -3,9 +3,8 @@ import re
 import numpy as np
 import math
 from torch.utils.data import Subset
-
-
-
+from g2p_en import G2p
+g2p = G2p()  # <- Global instance
 def convert_sentence(s):
     
     s = s.lower()
